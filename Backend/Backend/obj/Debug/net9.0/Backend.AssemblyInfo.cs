8afa0ccab776abac49adc4a74fc21c9409cee8d9
Ereshkigal <roma.hlinin@yandex.ru>
2025-01-19 23:33:35 +0300

    adding frontend

--- Backend/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730699801331f26312a9a27fbf82c7f5798e5df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b315007a96861c5a0f62527d5058c53bb88c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
